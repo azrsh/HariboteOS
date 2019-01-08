58be958a3dbacfdfb1fed29fdf5cf422d7a74e1c
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2019-01-08 20:44:22 +0900

    Marge

--- bootpack.c
+++ bootpack.c
@@ -8,27 +8,7 @@ void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int x_size, unsigned char color, int x0, int y0, int x1, int y1);
 
-<<<<<<< HEAD
 const unsigned char Color8_000000 = 0;
-=======
-#define COL8_000000 0
-#define COL8_FF0000 1
-#define COL8_00FF00 2
-#define COL8_FFFF00 3
-#define COL8_0000FF 4
-#define COL8_FF00FF 5
-#define COL8_00FFFF 6
-#define COL8_FFFFFF 7
-#define COL8_C6C6C6 8
-#define COL8_840000 9
-#define COL8_008400 10
-#define COL8_848400 11
-#define COL8_000084 12
-#define COL8_840084 13
-#define COL8_008484 14
-#define COL8_848484 15
-/*const unsigned char Color8_000000 = 0;
->>>>>>> 7a8fed4cef952a6780c2a85b5c15057f6edf85b9
 const unsigned char Color8_FF0000 = 1;
 const unsigned char Color8_00FF00 = 2;
 const unsigned char Color8_FFFF00 = 3;
@@ -43,7 +23,6 @@ const unsigned char Color8_848400 = 11;
 const unsigned char Color8_000084 = 12;
 const unsigned char Color8_840084 = 13;
 const unsigned char Color8_008484 = 14;
-<<<<<<< HEAD
 const unsigned char Color8_848484 = 15;
 
 void HariMain(void)
@@ -76,21 +55,6 @@ void HariMain(void)
     boxfill8(vram, x_size, Color8_848484, x_size - 47, y_size - 23, x_size - 47, y_size - 4);
     boxfill8(vram, x_size, Color8_FFFFFF, x_size - 47, y_size - 3, x_size - 4, y_size - 3);
     boxfill8(vram, x_size, Color8_FFFFFF, x_size - 3, y_size - 24, x_size - 3, y_size - 3);*/
-=======
-const unsigned char Color8_848484 = 15;*/
-
-void HariMain(void)
-{
-    char *p;
-
-    init_palette();
-
-    p = (char *)0xa0000;
-
-    boxfill8(p, 320, COL8_FF0000, 20, 20, 120, 120);
-    boxfill8(p, 320, COL8_00FF00, 70, 50, 170, 150);
-    boxfill8(p, 320, COL8_0000FF, 120, 80, 220, 180);
->>>>>>> 7a8fed4cef952a6780c2a85b5c15057f6edf85b9
 
     for (;;)
         io_hlt();
