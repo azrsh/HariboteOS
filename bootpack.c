7e2b9eae83784a00e5da696e14b1f70facdd8fa4
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2019-01-18 17:08:53 +0900

    5-9 GDTとIDTの初期化

--- bootpack.c
+++ bootpack.c
@@ -5,6 +5,8 @@ void io_cli(void);
 void io_out8(int port, int data);
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
+void load_gdtr(int limit, int adress);
+void load_idtr(int limit, int adress);
 
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
@@ -38,6 +40,24 @@ struct BOOTINFO
     short screenX, screenY;
     char *vram;
 };
+
+struct SEGMENT_DESCRIPTOR
+{
+    short limit_low, base_low;
+    char base_mid, access_right;
+    char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR
+{
+    short offset_low, selector;
+    char dw_count, access_right;
+    short offset_high;
+};
+
+void init_gdtidt(void);
+void set_segment_descriptor(struct SEGMENT_DESCRIPTOR *segmentDescriptor, unsigned int limit, int base, int access_right);
+void set_gate_descriptor(struct GATE_DESCRIPTOR *gateDescriptor, int offset, int selector, int access_right);
 
 void HariMain(void)
 {
@@ -225,5 +245,53 @@ void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py
         for (x = 0; x < pxsize; x++)
             vram[(py0 + y) * vxsize + (px0 + x)] = buffer[y * bxsize + x];
     }
+    return;
+}
+
+void init_gdtidt(void)
+{
+    struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *)0x00270000;
+    struct GATE_DESCRIPTOR *idt = (struct GATE_DESCRIPTOR *)0x0026f800;
+    int i = 0;
+
+    //GDTの初期化
+    for (i = 0; i < 8192; i++)
+        set_segment_descriptor(gdt + i, 0, 0, 0);
+    set_segment_descriptor(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
+    set_segment_descriptor(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
+    load_gdtr(0xffff, 0x0026f800);
+
+    //IDTの初期化
+    for (i = 0; i < 256; i++)
+        set_gate_descriptor(idt + i, 0, 0, 0);
+    load_idtr(0x7ff, 0x0026f800);
+
+    return;
+}
+
+void set_segment_descriptor(struct SEGMENT_DESCRIPTOR *segmentDescriptor, unsigned int limit, int base, int access_right)
+{
+    if (limit > 0xfffff)
+    {
+        access_right |= 0x8000; //G_bit = 1
+        limit /= 0x1000;
+    }
+
+    segmentDescriptor->limit_low = limit & 0xffff;
+    segmentDescriptor->base_low = base & 0xffff;
+    segmentDescriptor->base_mid = (base >> 16) & 0xff;
+    segmentDescriptor->access_right = access_right & 0xff;
+    segmentDescriptor->limit_high = ((limit >> 16) & 0x0f) | ((access_right >> 8) & 0xf0);
+    segmentDescriptor->base_high = (base >> 24) & 0xff;
+    return;
+}
+
+void set_gate_descriptor(struct GATE_DESCRIPTOR *gateDescriptor, int offset, int selector, int access_right)
+{
+    gateDescriptor->offset_low = offset & 0xffff;
+    gateDescriptor->selector = selector;
+    gateDescriptor->dw_count = (access_right >> 8) & 0xff;
+    gateDescriptor->access_right = access_right & 0xff;
+    gateDescriptor->offset_high = (offset >> 16) & 0xffff;
     return;
 }
