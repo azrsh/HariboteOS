ac5b8657b7ef99186a4aeeff558606d14a061613
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2019-01-20 01:37:50 +0900

    変数の命名規則を変更（統一）

--- bootpack.c
+++ bootpack.c
@@ -3,20 +3,20 @@
 
 void HariMain(void)
 {
-    struct BOOTINFO *boot_info = (struct BOOTINFO *)ADRESS_BOOTINFO; //boot infoの開始アドレス
-    char s[40], mouse_cursor[256];
-    int mouse_x, mouse_y;
+    struct BOOTINFO *bootInfo = (struct BOOTINFO *)ADRESS_BOOTINFO; //boot infoの開始アドレス
+    char s[40], mouseCursor[256];
+    int mouseX, mouseY;
 
     init_palette();
-    init_screen(boot_info->vram, boot_info->screenX, boot_info->screenY);
+    init_screen(bootInfo->vram, bootInfo->screenX, bootInfo->screenY);
 
-    mouse_x = (boot_info->screenX - 16) / 2; //画面中央に配置
-    mouse_y = (boot_info->screenY - 28 - 16) / 2;
-    init_mouse_cursor8(mouse_cursor, COLOR8_008484);
-    putblock8_8(boot_info->vram, boot_info->screenX, 16, 16, mouse_x, mouse_y, mouse_cursor, 16);
+    mouseX = (bootInfo->screenX - 16) / 2; //画面中央に配置
+    mouseY = (bootInfo->screenY - 28 - 16) / 2;
+    init_mouse_cursor8(mouseCursor, COLOR8_008484);
+    putblock8_8(bootInfo->vram, bootInfo->screenX, 16, 16, mouseX, mouseY, mouseCursor, 16);
 
-    sprintf(s, "(%d, %d)", mouse_x, mouse_y);
-    putfonts8_asc(boot_info->vram, boot_info->screenX, 0, 0, COLOR8_FFFFFF, s);
+    sprintf(s, "(%d, %d)", mouseX, mouseY);
+    putfonts8_asc(bootInfo->vram, bootInfo->screenX, 0, 0, COLOR8_FFFFFF, s);
 
     for (;;)
     {
