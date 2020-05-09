3288cb68193ffb18f31d435a1a213409b93674dd
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2020-05-09 19:58:24 +0900

    コメントの修正

--- int.c
+++ int.c
@@ -33,7 +33,7 @@ struct FIFO8 keyFifo;
 void inthandler21(int *esp)
 {
     unsigned char data;
-    io_out8(PIC0_OCW2, 0x61);   //PICに割り込みを受け取ったことを通知(IRQ1=0x61,IRQ3=0x63)
+    io_out8(PIC0_OCW2, 0x61); //PICに割り込みを受け取ったことを通知(IRQ1=0x61,IRQ3=0x63)
     data = io_in8(PORT_KEYAT);
     fifo8_put(&keyFifo, data);
     return;
@@ -45,14 +45,14 @@ void inthandler2c(int *esp)
     struct BOOTINFO *bootInfo = (struct BOOTINFO *)ADRESS_BOOTINFO;
     boxfill8(bootInfo->vram, bootInfo->screenX, COLOR8_000000, 0, 0, 32 * 8 - 1, 15);
     putfonts8_asc(bootInfo->vram, bootInfo->screenX, 0, 0, COLOR8_FFFFFF, "INT 2C (IRQ-12) : PS/2 mouse");
-    for(;;)
+    for (;;)
     {
         io_hlt();
     }
 }
 
 //PIC0からの不完全割り込み対策
-//Athlon64X2機などでは、チップセットの都合によりPICの初期化時に個の割り込みが起こるらしい
+//Athlon64X2機などでは、チップセットの都合によりPICの初期化時にこの割り込みが起こるらしい
 //この割り込みは、PIC初期化時の電気的ノイズによって発生するので、なんらかの処理を行う必要はない
 void inthandler27(int *esp)
 {
