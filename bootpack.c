a9b280b327453b008816523587b1261d362d910e
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2020-05-11 01:17:10 +0900

    9-4 メモリ管理用の構造体と関数を追加

--- bootpack.c
+++ bootpack.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include "bootpack.h"
 
+#define MEMMAN_FREES 4090      //約32KB
+#define MEMMAN_ADDR 0x003c0000 //メモリマネージャの先頭アドレス。ここから32KBがメモリマネージャの構造体
+
+struct FREEINFO
+{
+    unsigned int address, size;
+};
+
+struct MEMORYMANAGER
+{
+    int frees, maxfrees, lostSize, losts;
+    struct FREEINFO freeInfo[MEMMAN_FREES];
+};
+
 unsigned int memory_test(unsigned int start, unsigned int end);
+void memorymanager_init(struct MEMORYMANAGER *memorymanager);
+unsigned int memorymanager_total(struct MEMORYMANAGER *memorymanager);
+unsigned int memorymanager_allocate(struct MEMORYMANAGER *memorymanager, unsigned int size);
+int memorymanager_free(struct MEMORYMANAGER *memorymanager, unsigned int address, unsigned int size);
 
 void HariMain(void)
 {
     struct BOOTINFO *bootInfo = (struct BOOTINFO *)ADRESS_BOOTINFO; //boot infoの開始アドレス
     char s[40], mouseCursor[256], keyBuffer[32], mouseBuffer[128];
     int mouseX, mouseY, i;
+    unsigned int memoryTotal;
     struct MOUSE_DECODE mouseDecode;
+    struct MEMORYMANAGER *memoryManager = (struct MEMORYMANAGER *)MEMMAN_ADDR;
 
     init_gdtidt();
     init_pic();
@@ -23,6 +43,11 @@ void HariMain(void)
     init_keyboard();
     enable_mouse(&mouseDecode);
 
+    memoryTotal = memory_test(0x00400000, 0xbfffffff);
+    memorymanager_init(memoryManager);
+    memorymanager_free(memoryManager, 0x00001000, 0x0009e000); // 0x00001000 - 0x0009efff
+    memorymanager_free(memoryManager, 0x00400000, memoryTotal - 0x00400000);
+
     init_palette();
     init_screen(bootInfo->vram, bootInfo->screenX, bootInfo->screenY);
     mouseX = (bootInfo->screenX - 16) / 2; //画面中央に配置
@@ -32,8 +57,7 @@ void HariMain(void)
     sprintf(s, "(%d, %d)", mouseX, mouseY);
     putfonts8_asc(bootInfo->vram, bootInfo->screenX, 0, 0, COLOR8_FFFFFF, s);
 
-    i = memory_test(0x00400000, 0xbfffffff) / (1024 * 1024);
-    sprintf(s, "memory %dMB", i);
+    sprintf(s, "memory %dMB    free : %dKB", memoryTotal / (1024 * 1024), memorymanager_total(memoryManager) / 1024);
     putfonts8_asc(bootInfo->vram, bootInfo->screenX, 0, 32, COLOR8_FFFFFF, s);
 
     for (;;)
@@ -139,4 +163,123 @@ unsigned int memory_test(unsigned int start, unsigned int end)
     }
 
     return i;
+}
+
+void memorymanager_init(struct MEMORYMANAGER *memorymanager)
+{
+    memorymanager->frees = 0;    //空き情報の個数
+    memorymanager->maxfrees = 0; //状態監視用。freesの最大値
+    memorymanager->lostSize = 0; //解放に失敗した合計サイズ
+    memorymanager->losts = 0;    //解放に失敗した回数
+    return;
+}
+
+unsigned int memorymanager_total(struct MEMORYMANAGER *memorymanager)
+{
+    unsigned int i, total = 0;
+    for (i = 0; i < memorymanager->frees; i++)
+    {
+        total += memorymanager->freeInfo[i].size;
+    }
+    return total;
+}
+
+//メモリ確保
+unsigned int memorymanager_allocate(struct MEMORYMANAGER *memorymanager, unsigned int size)
+{
+    unsigned int i, address;
+    for (i = 0; i < memorymanager->frees; i++)
+    {
+        if (memorymanager->freeInfo[i].size >= size)
+        {
+            //十分な広さの空きメモリを発見
+            address = memorymanager->freeInfo[i].address;
+            memorymanager->freeInfo[i].address += size;
+            memorymanager->freeInfo[i].size -= size;
+            if (memorymanager->freeInfo[i].size == 0)
+            {
+                //freeInfo[i]が空になったので前に詰める
+                memorymanager->frees--;
+                for (; i < memorymanager->frees; i++)
+                {
+                    memorymanager->freeInfo[i] = memorymanager->freeInfo[i + 1];
+                }
+            }
+            return address;
+        }
+    }
+
+    return 0; //空きがない
+}
+
+//メモリ解放
+int memorymanager_free(struct MEMORYMANAGER *memorymanager, unsigned int address, unsigned int size)
+{
+    int i, j;
+    //まとめやすさを考えるとfree[]がアドレス順のほうが都合がいいので挿入時にソート
+    for (i = 0; i < memorymanager->frees; i++)
+    {
+        if (memorymanager->freeInfo[i].address > address)
+        {
+            break;
+        }
+    }
+    //この時点でfree[i-1].address < address < freeInfo[i].addressのはず
+    if (i > 0) //前に空き領域が存在するとき
+    {
+        if (memorymanager->freeInfo[i - 1].address + memorymanager->freeInfo[i - 1].size == address)
+        {
+            memorymanager->freeInfo[i - 1].size += size; //前の空き領域の終端と開放する領域の先頭が隣接しているので統合
+            if (i < memorymanager->frees)                //後ろに空き領域が存在するとき
+            {
+                if (address + size == memorymanager->freeInfo[i].address)
+                {
+                    //開放する領域の終端と後ろの領域の先頭が隣接しているので統合する
+                    memorymanager->freeInfo[i - 1].size += memorymanager->freeInfo[i].size;
+
+                    //memorymanager->freeInfo[i]を削除して詰める
+                    memorymanager->frees--;
+                    for (; i < memorymanager->frees; i++)
+                    {
+                        memorymanager->freeInfo[i] = memorymanager->freeInfo[i + 1];
+                    }
+                }
+            }
+
+            return 0; //成功
+        }
+    }
+
+    //前の空き領域と統合できなかったとき
+    if (i < memorymanager->frees) //後ろに空き領域が存在するとき
+    {
+        if (address + size == memorymanager->freeInfo[i].address) //開放する領域の終端と後ろの領域の先頭が隣接しているので統合する
+        {
+            memorymanager->freeInfo[i].address = address;
+            memorymanager->freeInfo[i].size += size;
+            return 0; //成功
+        }
+    }
+
+    //前にも後ろにまとめられないとき
+    if (memorymanager->frees < MEMMAN_FREES)
+    {
+        //i以降の要素をすべてずらして隙間を作る
+        for (j = memorymanager->frees; j > i; j--)
+        {
+            memorymanager->freeInfo[j] = memorymanager->freeInfo[j - 1];
+        }
+        memorymanager->frees++;
+        if (memorymanager->maxfrees > memorymanager->frees)
+        {
+            memorymanager->maxfrees = memorymanager->frees; //最大値の更新
+        }
+        memorymanager->freeInfo[i].address = address;
+        memorymanager->freeInfo[i].size = size;
+        return 0; //成功
+    }
+
+    memorymanager->losts++;
+    memorymanager->lostSize += size;
+    return -1; //失敗
 }
